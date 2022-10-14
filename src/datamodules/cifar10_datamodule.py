e93a27bb175775a4e3dbe92a5015ec26bbf3b5d9
rampfire <abishek.ur@gmail.com>
2022-10-14 21:09:20 +0530

    revert to old configs

--- src/datamodules/cifar10_datamodule.py
+++ src/datamodules/cifar10_datamodule.py
@@ -31,23 +31,12 @@ class LitCIFAR10DataModule(LightningDataModule):
         return dataset
 
     def train_dataloader(self):
-        # transform = T.Compose(
-        #     [
-        #         T.RandomCrop(32, padding=4),
-        #         T.RandomHorizontalFlip(),
-        #         T.ToTensor(),
-        #         T.Normalize(self.mean, self.std),
-        #     ]
-        # )
-        transform = A.Compose(
+        transform = T.Compose(
             [
-                A.Normalize(mean=[0.49139968,0.48215841,0.44653091], std=[0.49139968,0.48215841,0.44653091]),
-                # A.Rotate(limit = (-7,7),always_apply = True),
-                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.50, rotate_limit=(-7,7), p=.75),
-                A.CoarseDropout (max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=[0.49139968,0.48215841,0.44653091],p = 0.75),
-                # A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=[1,1,1], always_apply=True, p=0.5),
-                A.HorizontalFlip(p=0.75),
-                ToTensorV2()
+                T.RandomCrop(32, padding=4),
+                T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Normalize(self.mean, self.std),
             ]
         )
         dataset = self.get_dataset(
